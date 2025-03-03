5d9f9471e5a92e2bb9c665ad343fd0f5e752081d
Sohan1818 <sohanshetty18@gmail.com>
2025-03-03 13:24:06 +0530

    Added Render URL to ALLOWED_HOSTS

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-@e*az8q-k#&(i9&r1170ee4cv0gmq8bno76g2$(=h!@#)9wv$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',  # Local development
+    'password-generator-udei.onrender.com'  # Your Render URL
+]
 
 
 # Application definition
