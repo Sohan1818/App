a36c40fd6a89e218d8a6536f815aefd45e0e0204
Sohan1818 <sohanshetty18@gmail.com>
2025-03-03 12:41:49 +0530

    Added missing project files

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,30 @@
+import random
+import string
+
 from django.shortcuts import render
 
-# Create your views here.
+def generate_password(length=8, use_letters=True, use_numbers=False, use_special=False):
+    char_pool = ''
+    if use_letters:
+        char_pool += string.ascii_letters
+    if use_numbers:
+        char_pool += string.digits
+    if use_special:
+        char_pool += string.punctuation
+    
+    if not char_pool:
+        char_pool = string.ascii_letters  # Default to letters if nothing is selected
+    
+    return ''.join(random.choice(char_pool) for _ in range(length))
+
+def home(request):
+    password = ''
+    if request.method == "POST":
+        length = int(request.POST.get("length", 8))
+        use_letters = "letters" in request.POST
+        use_numbers = "numbers" in request.POST
+        use_special = "special" in request.POST
+        
+        password = generate_password(length, use_letters, use_numbers, use_special)
+    
+    return render(request, "index.html", {"password": password})
--- password_generator/settings.py
+++ password_generator/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'generator',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'password_generator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'generator/templates')],  # Ensure this line exists
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'generator/static'),  # Adjust if your app has a different name
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
