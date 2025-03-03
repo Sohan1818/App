327c9436e4bbb6aeab542ade3a0f2e111a185871
Sohan1818 <sohanshetty18@gmail.com>
2025-03-03 14:36:43 +0530

    Fix static file path for deployment

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -24,12 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@e*az8q-k#&(i9&r1170ee4cv0gmq8bno76g2$(=h!@#)9wv$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',  # Local development
-    'password-generator-udei.onrender.com'  # Your Render URL
-]
 
 
 # Application definition
@@ -126,6 +121,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'generator/static'),  # Adjust if your app has a different name
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',  # Local development
+    'password-generator-udei.onrender.com'  # Your Render URL
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
